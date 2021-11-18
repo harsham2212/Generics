1ae0c621a786860a636bf39b67829a23f590795e
Harsh Singh <kmrharsh50@gmail.com>
2021-11-18 23:28:28 +0530

    [Harsh]Added:Find Maximum Integer between 3 Numbers Generics Implemented

---
+++ Generic/LargestInteger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generic
+{
+    class LargestInteger
+    {
+        public static int MaxNumber(int firstNum, int secondNum, int thirdNum)
+        {
+            if (firstNum.CompareTo(secondNum) > 0 && firstNum > thirdNum)
+            {
+                return firstNum;
+            }
+            if (secondNum.CompareTo(firstNum) > 0 && secondNum > thirdNum)
+            {
+                return secondNum;
+            }
+            else
+            {
+                return thirdNum;
+            }
+        }
+    }
+}
---
+++ Generic/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Generic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.WriteLine("Welcome To Generic Problems");
+                bool flag = true;
+                while (flag)
+                {
+                    Console.WriteLine("Choose Options: \n 1.find maximum integer\n 2.exit");
+                    int option = Convert.ToInt32(Console.ReadLine());
+                    switch (option)
+                    {
+                        case 1:
+                            Console.WriteLine("Enter Three Number to find:");
+                            int first = Convert.ToInt32(Console.ReadLine());
+                            int second = Convert.ToInt32(Console.ReadLine());
+                            int third = Convert.ToInt32(Console.ReadLine());
+                            int max = LargestInteger.MaxNumber(first, second, third);
+                            Console.WriteLine("Max Number is: " + max);
+                            break;
+                        case 2:
+                            flag = false;
+                            break;
+                    }
+                }
+
+            }
+        }
+    }
+}
